6b5bd4bf3d03fcc006225de530ab2f408c1b2d5d
luisbaldissera <luis.baldissera26@gmail.com>
2025-05-09 22:04:01 +0200

    test: updated test json manipulation

--- tests/test_json_manipulation.c
+++ tests/test_json_manipulation.c
@@ -1,38 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "libjson/json.h"
-#include "test_utils.h"
 
 void test_json_array_manipulation()
 {
-    json array = json_array();
-    json value1 = json_integer(1);
-    json value2 = json_integer(2);
+    struct json *array = json_array();
 
+    // Create the values
+    struct json *value1 = json_number(10);
+    struct json *value2 = json_number(20);
+
+    // Push to array (array takes ownership)
     json_array_push(array, value1);
     json_array_push(array, value2);
 
-    TEST_ASSERT_EQUAL_INT(2, JSON_array_length(array));
-    TEST_ASSERT_EQUAL_INT(1, JSON_integer_value(JSON_array_get(array, 0)));
-    TEST_ASSERT_EQUAL_INT(2, JSON_integer_value(JSON_array_get(array, 1)));
 
-    JSON_free(value1);
-    JSON_free(value2);
-    JSON_free(array);
+    // Test the array
+    assert(json_array_length(array) == 2);
+    assert(json_toint(json_array_get(array, 0)) == 10);
+    assert(json_toint(json_array_get(array, 1)) == 20);
+
+    // Free the array (and all its contents)
+    json_free(array);
 }
 
 void test_json_object_manipulation()
 {
-    json object = json_object();
-    json value = json_string("test");
+    struct json *object = json_object();
 
+    // Create the value
+    struct json *value = json_string("test");
+
+    // Set on object (object takes ownership)
     json_object_set(object, "key", value);
 
-    TEST_ASSERT_EQUAL_STRING("test", JSON_string_value(json_object_get(object, "key")));
 
-    JSON_free(value);
-    JSON_free(object);
+    // Test the object
+    assert(json_object_get(object, "key") != NULL);
+    assert(json_isstring(json_object_get(object, "key")));
+    assert(json_tostring(json_object_get(object, "key")) != NULL);
+    assert(strcmp(json_tostring(json_object_get(object, "key")), "test") == 0);
+
+    // Free the object (and all its contents)
+    json_free(object);
 }
 
 int main()
